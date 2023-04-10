a0ca9c58b77bf33e11a3f01c2bc11d5ad7de3441
Velwitch <phil.pynson@gmail.com>
2023-04-10 13:20:09 +0200

    vue Roles done

--- afpa/resources/views/dashboard.blade.php
+++ afpa/resources/views/dashboard.blade.php
@@ -1,3 +1,8 @@
+@php
+$user = Auth::user();
+$roles = $user->roles;
+@endphp
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -9,7 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("You're logged in!") }} @php if($roles[0]->nom == 'Super Admin') { echo 'Super Admin'; } @endphp
                 </div>
                 <form id="recetteForm" class="px-5 " action="{{ route('evenements.index') }}" method="GET" enctype="multipart/form-data">
             @csrf
